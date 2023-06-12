395863c2803f3093ea436ddfa656686305aa370f
Red-Rim <salmaouakhir20@gmail.com>
2023-06-12 01:44:24 -0700

    .

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        if tens_digit < 8:
-            print("{:d}{:d}, ".format(tens_digit, ones_digit), end="")
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end='')
+        if i != 8 or j != 9:
+            print(", ", end='')
         else:
-            print("{:d}{:d}".format(tens_digit, ones_digit))
-
-print()
+            print()
