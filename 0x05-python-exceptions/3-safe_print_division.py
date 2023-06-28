409daf6ac63b1b2904e6d159171a741a4fdc8d9c
Red-Rim <salmaouakhir20@gmail.com>
2023-06-28 16:48:12 -0700

    .

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
         div = a / b
-    except ZeroDivisionError:
+        except ZeroDivisionError:
         div = None
     finally:
         print("Inside result: {}".format(div))
