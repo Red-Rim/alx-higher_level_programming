73729479d4fb8ecce8976e57faf5a84182433f0f
Red-Rim <salmaouakhir20@gmail.com>
2023-06-29 18:38:25 -0700

    .

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Define a class Squar"""
+
+
+class Square:
+    """Represent a squar"""
+
+    def __init__(self, size=0):
+        """Init a new squar
+
+        Args:
+            size (int): size of the new squar
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return current area of the squar"""
+        return (self.__size * self.__size)
