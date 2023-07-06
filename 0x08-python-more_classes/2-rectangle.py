90a7e89ccb461081a4746b5d342fc8632a0b0a40
Red-Rim <salmaouakhir20@gmail.com>
2023-07-05 17:44:06 -0700

    .

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
-"""
-Module 2-rectangle
-"""
+""" Define a rectangle class"""
 
 
 class Rectangle:
-    """
-    Defines class rectangle with private attribute width and height
-
-    Args:
-        width (int): width
-        height (int): height
-
-    Functions:
-        __init__(self, width, height)
-        width(self)
-        width(self, value)
-        height(self)
-        height(self, value)
-        area(self)
-        perimeter(self)
-    """
+    """Defines class rectangle"""
     def __init__(self, width=0, height=0):
         """ Initialize rectangles """
         self.width = width
