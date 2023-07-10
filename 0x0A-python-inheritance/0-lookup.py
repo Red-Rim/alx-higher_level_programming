ed43f6291246f25f69d267c6514e9a90cffac233
Red-Rim <salmaouakhir20@gmail.com>
2023-07-10 16:10:43 -0700

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-    """a function that returns the list"""
+"""function that that returns the list"""
 
 
 def lookup(obj):
-    """  returns the list of available attributes and methods of an object
-         return: list """
+    """returns the list of available attributes and methods
+       obj : argument
+       Return : list
+    """
 
-    return dir(obj)
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/0-lookup0.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+    """a function that returns the list"""
+
+
+def lookup(obj):
+    """  returns the list of available attributes and methods of an object
+         return: list """
+
+    return dir(obj)
