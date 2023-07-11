27b1c18fd8cd16e187258e750ad5df04f5ab8ad1
Red-Rim <salmaouakhir20@gmail.com>
2023-07-11 14:19:29 -0700

    .

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,8 +4,8 @@
 
 
 def read_file(filename=""):
-    """function that reads a text file (UTF8) and prints it to stdou"""
-     with open(filename, encoding='utf-8') as file:
+    """function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding='utf-8') as file:
         for line in file:
             print(line, end='')
 
