9b5306ffb408afdc4ad7c27398c04930b14d451f
Red-Rim <salmaouakhir20@gmail.com>
2023-07-15 12:36:01 -0700

    .

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+class MyList(list):
+"""MyList that inherits from list"""
+
+
+
+    def print_sorted(self):
+        """ a class MyList that inherits from list"""
+        sorted_list = sorted(self)
+        print(sorted_list)
+
