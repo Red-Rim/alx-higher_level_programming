e0020316809fd7f5e24b8ee79d8b6e1cac680f2b
Red-Rim <salmaouakhir20@gmail.com>
2023-07-16 12:12:17 -0700

    .

--- 0x0A-python-inheritance/6-main.py
+++ 0x0A-python-inheritance/6-main.py
@@ -4,7 +4,8 @@ BaseGeometry = __import__('6-base_geometry').BaseGeometry
 bg = BaseGeometry()
 
 try:
-        print(bg.area())
+    print(bg.area())
 except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    print("[{}] {}".format(e.__class__.__name__, e))
+
 
