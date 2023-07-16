985ab0ee658d389615c364a581af74fa25d52f45
Red-Rim <salmaouakhir20@gmail.com>
2023-07-16 12:18:20 -0700

    .

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""build a class"""
+
+
+class BaseGeometry:
+    """class building"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
