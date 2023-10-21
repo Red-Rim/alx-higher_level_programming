aa5ae0f3d1b1ef5e209de762cc43eab2ba0f1705
Red-Rim <salmaouakhir20@gmail.com>
2023-10-21 01:42:26 +0100

    first_table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- script that creates a table called first_table in the current database
-CRATE TABLE IF NOT EXISTS first_table (
-	id int,
-	name VARCHAR(256)
-	);
+CREATE TABLE IF NOT EXISTS first_tabl (id INT, name varchar(256));
--- 4-first_table.sql
+++
@@ -1,2 +0,0 @@
--- script that creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS first_tabl (id INT, name varchar(256));
