ee6a8bd06b7e002f03497bcbbdcf23299a1211ed
Red-Rim <salmaouakhir20@gmail.com>
2023-12-01 14:06:02 +0100

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email address, sends a POST request"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    if (r.status_code >= 400):
+        print(f"Error code: {r.status_code}")
+        exit()
+    print(r.text)
